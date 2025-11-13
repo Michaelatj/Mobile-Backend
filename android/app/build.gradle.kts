d22ef7c2a0417f4bee808d53adc819aafb948d6c
Michael Andreas Tjendra <alongbest2000@gmail.com>
2025-11-13 18:29:18 +0700

    update all files

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.bantuin_silvani"
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
